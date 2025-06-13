0f34f3d15c3782044810f5e2e9d83cdf928baccc
interlokutor1 <06.samuel.lundblad@gmail.com>
2025-06-13 21:08:50 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -4,7 +4,15 @@ function uppg5() {
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
 
-
+let tal1 = 1;
+    let tal2 = 2;
+    
+    if(tal1 > tal2){
+        console.log("Det första talet är större än det andra");
+    }  
+    else{
+        console.log("Det andra talet är större än det första");
+    }
 
 }
 
