3827abb3bddd246bb91ab50700f97081af78df04
interlokutor1 <06.samuel.lundblad@gmail.com>
2025-06-13 21:09:33 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -5,7 +5,13 @@ function uppg7() {
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+function addera(tal1, tal2){
+        return tal1 + tal2;
+    }
 
+    let summa = addera(5, 10);
+    console.log(summa);
+}
  
 }
 
