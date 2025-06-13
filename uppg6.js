82e8c586ae0752f84b605f1bfd139c8fe5cc654a
interlokutor1 <06.samuel.lundblad@gmail.com>
2025-06-13 21:09:14 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -4,7 +4,11 @@ function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
+let names = ["Samuel", "Joakim", "William", "Eddie"];
 
+    for(let i = 0; i < names.length; i++){
+        console.log(names.i);
+    }
  
 }
 
