a47b3bfb2dec78bd2f76d36cad123292de335c93
interlokutor1 <06.samuel.lundblad@gmail.com>
2025-06-13 21:41:01 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -3,7 +3,7 @@ function uppg2() {
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
 let namn = "samuel";
-   console.log(X);
+   console.log(namn);
 
 }
 
