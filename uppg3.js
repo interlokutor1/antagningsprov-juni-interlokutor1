b0495fa4cd4c3567b36559fa456f3d5546dea7f7
interlokutor1 <06.samuel.lundblad@gmail.com>
2025-06-13 21:08:05 +0200

    Update uppg3.js

--- uppg3.js
+++ uppg3.js
@@ -6,7 +6,9 @@ function uppg3() {
     // deklarera två variabler, a och b
     // tilldela värdena 5 och 10 till variablerna
     // skriv ut summan av a och b i konsolen
-
+let a = 5;
+    let b = 10;
+    console.log(a + b);
 
 
 }
